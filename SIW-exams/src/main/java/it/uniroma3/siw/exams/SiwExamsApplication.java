883a64dc28aaa89ebe911a688bdc4ff02f3b88a5
hccsan <72209607+hccsan@users.noreply.github.com>
2022-08-16 17:39:39 +0200

    initial files

---
+++ SIW-exams/src/main/java/it/uniroma3/siw/exams/SiwExamsApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwExamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwExamsApplication.class, args);
+	}
+
+}
---
+++ SIW-exams/src/test/java/it/uniroma3/siw/exams/SiwExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwExamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
